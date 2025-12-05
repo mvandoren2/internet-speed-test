e3878cf37e404fcc6f1562eb84a9b9651b20dca5
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2025-12-05 09:29:25 -0500

    Update fofaina-speed-loop.ps1

--- fofaina-speed-loop.ps1
+++ fofaina-speed-loop.ps1
@@ -2,12 +2,30 @@ $startTime = Get-Date -Format "yyyy-MM-dd_HH-mm"
 $destinationStageFile = Join-Path -Path $PWD -ChildPath "speed-tests_$startTime.csv"
 $destinationFile = Join-Path -Path $PWD -ChildPath "speed-tests-converted_$startTime.csv"
 $exePath = Join-Path -Path $PWD -ChildPath "speedtest.exe"
+$zipUrl = "https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-win64.zip"   # URL of the zip file
+$zipPath = Join-Path -Path $PWD -ChildPath "ookla-speedtest-1.2.0-win64.zip"
+
+if (-Not (Test-Path $exePath)) {
+    Write-Host "File not found. Taking action..."
+    
+    Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+    Expand-Archive -Path $zipPath -DestinationPath $PWD -Force
+    Remove-Item -Path $zipPath -Force
+
+    
+    # Or run any other command you want here
+    # e.g., Send-MailMessage, log entry, etc.
+} else {
+    Write-Output "File exists. No action needed."
+}
+
+
 
 $headerOutput = & $exePath "-f" "csv" "--output-header"
 
 $headerOutput | Out-File -FilePath $destinationStageFile -Append -Encoding utf8
 
-for ($i = 1; $i -le 5; $i++) {
+for ($i = 1; $i -le 2; $i++) {
 
     Start-Sleep -Seconds 30
 
